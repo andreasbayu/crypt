d26d85a5038bc695a77f90cc122cabdafd3e8ca0
andreasbayu <andreasbayu1007@gmail.com>
2023-03-15 14:42:02 +0700

    update: allow pkg dict to commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+## Installation
+```bash
+npm install
+```
+
+```bash
+yarn add -D vite-plugin-wasm
+```
---
+++ libs/pkg/libs.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} text
+* @param {number} shift
+* @returns {string}
+*/
+export function caesar_encode(text: string, shift: number): string;
+/**
+* @param {string} text
+* @param {number} shift
+* @returns {string}
+*/
+export function caesar_decode(text: string, shift: number): string;
